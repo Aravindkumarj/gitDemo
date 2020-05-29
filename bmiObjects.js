3cb2f6f576169a952c29dc216c50ef4f7c98a7d9
Aravind Kumar Jagannathan <aravind.kumar.jagannathan@sap.com>
2020-05-28 22:36:42 -0500

    Change id: 12345 : Added new property

---
+++ bmiObjects.js
@@ -0,0 +1,27 @@
+var Mike = {
+	fullName : 'Mike Tyson',
+	pet : 'Tiger',
+	mass : 300,
+	height : 5.11,
+	calcBMI : function(){
+		this.BMI = this.mass / (this.height * this.height);
+		return this.BMI;
+	}
+}
+var John = {
+	fullName : 'John Cena',
+	pet : 'Dog',
+	mass : 275,
+	height : 6.0,
+	calcBMI : function(){
+		this.BMI = this.mass / (this.height * this.height);
+		return this.BMI;
+	}
+}
+
+Mike.calcBMI();
+John.calcBMI();
+
+console.log((Mike.BMI > John.BMI) ? ("Mike's BMI" + " " + Mike.BMI + " " + "is greater than Johns" + " " + John.BMI) : ((Mike.BMI == John.BMI) ? "They both have the same BMI" : "John's BMI is greater than Mikes"));
+console.log (Mike)
+console.log (John)
